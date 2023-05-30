8e3729f4df4ad792f5b4e4ec9175701fd3765a97
div-antk <51283468+owaii@users.noreply.github.com>
2023-05-30 21:28:43 +0900

    Initial Commit

---
+++ TcaSwiftUI/TcaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TcaSwiftUIApp.swift
+//  TcaSwiftUI
+//
+//  Created by Takuya Ando on 2023/05/30.
+//
+
+import SwiftUI
+
+@main
+struct TcaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
