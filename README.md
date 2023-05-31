68a1cb03d3c80ad7c179a12e962587c47885b7e2
div-antk <1984ta@gmail.com>
2023-05-31 23:59:00 +0900

    TCAパッケージのインストール

---
+++ README.md
@@ -0,0 +1,3 @@
+# TcaSwiftUI
+
+A description of this package.
---
+++ Sources/TcaSwiftUI/TcaSwiftUI.swift
@@ -0,0 +1,8 @@
+@main
+public struct TcaSwiftUI {
+    public private(set) var text = "Hello, World!"
+
+    public static func main() {
+        print(TcaSwiftUI().text)
+    }
+}
