9efc302bfa3a3548260140b9c25ded0acfd441a4
div-antk <1984ta@gmail.com>
2023-06-01 20:33:04 +0900

    うまくいかない

--- TcaSwiftUI/ContentView.swift
+++ TcaSwiftUI/ContentView.swift
@@ -6,21 +6,60 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
+// アプリの状態
+struct AppState {
+    var count: Int = 0
+}
+
+// アクション
+enum AppAction {
+    case increment
+    case decrement
+}
+
+// アクションを受け取り、状態の更新を行うリダクション
+let appReducer = Reducer<AppState, AppAction, Void> { state, action, _ in
+    switch action {
+    case .increment:
+        state.count += 1
+        return .none
+        
+    case .decrement:
+        state.count -= 1
+        return .none
+    }
+}
+
+// ビュー
 struct ContentView: View {
+    let store: Store<AppState, AppAction>
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        WithViewStore(store) { viewStore in
+            VStack {
+                Text("Count: \(viewStore.count)")
+                HStack {
+                    Button(action: { viewStore.send(.decrement) }) {
+                        Text("Decrement")
+                    }
+                    Button(action: { viewStore.send(.increment) }) {
+                        Text("Increment")
+                    }
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(store: Store(
+            initialState: AppState(),
+            reducer: appReducer,
+            environment: ()
+        ))
     }
 }
