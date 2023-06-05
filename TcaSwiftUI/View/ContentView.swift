670f50cba0693788a4ea41da54885f187f87ee8b
div-antk <1984ta@gmail.com>
2023-06-05 23:59:00 +0900

    Viewにおける状態部分の実装

---
+++ TcaSwiftUI/View/ContentView.swift
@@ -0,0 +1,92 @@
+//
+//  ContentView.swift
+//  TcaSwiftUI
+//
+//  Created by Takuya Ando on 2023/05/30.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct AppEnvironment {
+    var gitHubApi = GithubApi.live
+    var mainQueue: AnySchedulerOf<DispatchQueue> = .main
+}
+
+// アプリの状態
+struct AppState: Equatable {
+    var users = [User]()
+    var searchQuery: String = ""
+}
+
+// アクション
+enum AppAction: Equatable {
+    case searchQueryEditing(String)
+    case response(Result<[User], ModelError>)
+}
+
+// アクションを受け取り、状態の更新を行うリダクション
+let appReducer = Reducer<AppState, AppAction, AppEnvironment> { state, action, environment in
+    switch action {
+    case let .searchQueryEditing(query):
+        struct SearchUserId: Hashable {}
+        
+        return environment.gitHubApi
+            .users(query)
+            .receive(on: environment.mainQueue)
+            .catchToEffect(AppAction.response)
+            .cancellable(id: SearchUserId(),
+                         cancelInFlight: true
+            )
+    case let .response(.success(users)):
+        state.users = users
+        return .none
+    case let .response(.failure(error)):
+        print(error)
+        return .none
+    }
+}
+
+// ビュー
+struct ContentView: View {
+    let store: Store<AppState, AppAction>
+    
+    var body: some View {
+        WithViewStore(store) { viewStore in
+            NavigationView {
+                VStack {
+                    TextField(
+                        "user name",
+                        text: viewStore.binding(
+                            get: \.searchQuery,
+                            send: AppAction.searchQueryEditing
+                        )
+                    )
+                    
+                }
+            }
+            VStack {
+                Text("Count: \(viewStore.count)")
+                HStack {
+                    Button(action: { viewStore.send(.decrement) }) {
+                        Text("Decrement")
+                    }
+                    Button(action: { viewStore.send(.increment) }) {
+                        Text("Increment")
+                    }
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(store: Store(
+            initialState: AppState(),
+            reducer: appReducer,
+            environment: ()
+        ))
+    }
+}
